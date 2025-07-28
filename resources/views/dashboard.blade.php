dcd301bc58f24615e9ade694343896497064f8d6
azkila <azkila.071218@gmail.com>
2025-07-28 11:38:59 +0700

    Mengubah Tulisan

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="text-xl font-semibold leading-tight text-blue-600">
+        <h2 class="text-xl font-semibold leading-tight text-gray-600">
             {{ __('Dashboard') }}
         </h2>
     </x-slot>
@@ -9,7 +9,7 @@
         <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
             <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
+                    {{ __("Assalamualaikum github") }}
                 </div>
             </div>
         </div>
